5390ef85498e48eb8f783b04dd9b815e7e18dfd8
Stefan Kostoski <72026527+stefankostoski@users.noreply.github.com>
2022-08-31 20:52:01 +0200

    .

--- notes-app-client/src/app/app.module.ts
+++ notes-app-client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
